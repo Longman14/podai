e28ff22fd301ed2c989d72b64735c95b07d01d63
longman14 <hassandamilola580@gmail.com>
2024-11-25 07:05:37 +0000

    configured convex and clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers:[
+        {
+            domain: "https://major-kitten-90.clerk.accounts.dev",
+            applicationID: "convex"
+        }
+    ]
+}
+
+export default authConfig;
